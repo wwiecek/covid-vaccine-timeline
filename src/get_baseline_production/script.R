177fd34dd123d354f02fd4902b64b397b084029c
Tomas Dulka <tomasdulka@gmail.com>
2022-11-29 17:43:44 +0300

    produce a plot of distributed vs administered vaccines in US

--- src/get_baseline_production/script.R
+++ src/get_baseline_production/script.R
@@ -1,12 +1,18 @@
+# # Use to switch into this file's directory for development
+# root = here::here()
+# setwd(file.path(root, "src/get_baseline_production"))
+# setwd(root) # Use to get back to root
+
 usa_raw_data = read.csv("COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv")
 
 usa_distributed_vaccines_by_state = usa_raw_data %>%
   mutate(date = as.Date(Date, format = "%m/%d/%Y")) %>%
-  select(date, Location, Distributed)
+  select(date, Location, Distributed, Administered)
 
 usa_available_vaccines_series = usa_distributed_vaccines_by_state %>%
   group_by(date) %>%
-  summarise(cumulative_available_vaccines = sum(Distributed)) %>%
+  summarise(cumulative_available_vaccines = sum(Distributed),
+            cumulative_administered_vaccines = sum(Administered)) %>%
   # before this date the observations are daily, afterwards weekly
   # keeping the daily section
   filter(date < "2022-06-22") %>%
@@ -20,6 +26,15 @@ uk_data = data.frame(
   iso3c = "GBR"
 )
 
-baseline_vaccine_production = bind_rows(usa_available_vaccines_series, uk_data)
+baseline_vaccine_production = bind_rows(usa_available_vaccines_series, uk_data) %>%
+  select(date, cumulative_available_vaccines, country, iso3c)
 
 saveRDS(baseline_vaccine_production, "baseline_vaccine_production.Rds")
+
+distributed_vs_administered = ggplot(data = usa_available_vaccines_series) +
+  geom_line(aes(x = date, y = cumulative_available_vaccines, color = "Distributed")) +
+  geom_line(aes(x = date, y = cumulative_administered_vaccines, color = "Administered")) +
+  ylab('vaccines distributed / available') +
+  scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6))
+
+ggsave("CDC_distributed_vs_administered.pdf", distributed_vs_administered, device = "pdf")
