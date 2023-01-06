a01e28624bed6280e8d2c429073e1087df1ad563
Tomas Dulka <tomasdulka@gmail.com>
2023-01-06 16:51:20 -0300

    plot binding of production

--- src/generate_vaccine_counterfactual/script.R
+++ src/generate_vaccine_counterfactual/script.R
@@ -10,7 +10,6 @@
 #' @param shift_days how many days into the past should it move vaccination
 gen_cfact_with_prod = function(base_series, prod_series, shift_days) {
   dose2_delay = 21 # how many days after first shot can a person get second
-  dose2_max_share = 0.5 # 2nd doses can only take up to this share
 
   # contains the basic interest for vaccination with vaccines available sooner
   # combined with how many vaccines are available until each day
@@ -21,40 +20,29 @@ gen_cfact_with_prod = function(base_series, prod_series, shift_days) {
     mutate(first_doses_cfact = 0,
            second_doses_cfact = 0)
 
-  cumulative_vac = 0 # keep track of number of vaccines administered so far
-  dose1_overflow = 0 # how many people want a vaccine from base data but didn't get yet
-
+  # keep track of how many first doses were administered so far, second shots get
+  # reserved when someone comes for the first vaccination
+  cumulative_vac = 0
   for (i in 1:nrow(base_shifted_with_prod)) {
-    available_vac = base_shifted_with_prod$cumulative_available_vaccines[i] - cumulative_vac
+    doses_cap = base_shifted_with_prod$cumulative_available_vaccines[i] - cumulative_vac
+    distribution_cap = base_shifted_with_prod$first_doses[i] + base_shifted_with_prod$second_doses[i]
+    available_vac = floor(min(doses_cap, distribution_cap) / 2)
 
-    dose1_interest = base_shifted_with_prod$first_doses[i] + dose1_overflow
-    dose2_interest = 0
+    dose1_vaccinated = available_vac
+
+    dose2_vaccinated = 0
     if (i > dose2_delay) {
-      first_eligible_for_2 = sum(base_shifted_with_prod$first_doses_cfact[1:(i - dose2_delay)])
-      dose2_base_interest = sum(base_shifted_with_prod$second_doses[1:i])
-      dose2_cfact_given = sum(base_shifted_with_prod$second_doses_cfact[1:(i-1)])
-
-      dose2_interest = min(first_eligible_for_2, dose2_base_interest) - dose2_cfact_given
+      dose2_vaccinated = base_shifted_with_prod$first_doses_cfact[i - dose2_delay]
     }
-
-    # dose2 should left something for dose1
-    dose2_vaccinated = floor(min(dose2_interest, available_vac * dose2_max_share))
-    dose1_vaccinated = floor(min(dose1_interest, available_vac - dose2_vaccinated))
-    # if dose1 doesn't take all available up, dose2 can take more if interested
-    if (available_vac - dose1_vaccinated - dose2_vaccinated > 0) {
-      dose2_vaccinated = floor(min(dose2_interest, available_vac - dose1_vaccinated))
-    }
-
-    dose1_overflow = dose1_interest - dose1_vaccinated
 
     base_shifted_with_prod$second_doses_cfact[i] = dose2_vaccinated
     base_shifted_with_prod$first_doses_cfact[i] = dose1_vaccinated
 
-    cumulative_vac = cumulative_vac + dose2_vaccinated + dose1_vaccinated
+    cumulative_vac = cumulative_vac + dose1_vaccinated * 2
   }
 
   # third doses stay the same (just shifted in time) since they most probably
-  # don't face production constraints
+  # don't face production or distribution constraints
   cfact_df = base_shifted_with_prod %>%
     mutate(first_doses = first_doses_cfact,
            second_doses = second_doses_cfact) %>%
@@ -70,8 +58,10 @@ plot_cfact = function(cfact, baseline, production) {
   countries = unique(cfact$iso3c)
 
   walk(countries, function(country_iso) {
-    country_cfact = cfact %>% filter(iso3c == country_iso)
-    country_base = baseline %>% filter(iso3c == country_iso)
+    country_cfact = cfact %>% filter(iso3c == country_iso) %>%
+      mutate(total_vacc = cumsum(first_doses + second_doses + third_doses))
+    country_base = baseline %>% filter(iso3c == country_iso) %>%
+      mutate(total_vacc = cumsum(first_doses + second_doses + third_doses))
     country_prod = production %>% filter(iso3c == country_iso)
 
     plot_dose1 = ggplot() +
@@ -94,8 +84,15 @@ plot_cfact = function(cfact, baseline, production) {
       scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6)) +
       labs(color=country_iso)
 
-    country_cfact = country_cfact %>%
-      mutate(total_vacc = cumsum(first_doses + second_doses + third_doses))
+    plot_total_dose = ggplot() +
+      geom_line(data = country_base,
+                aes(x = as.Date(date), y = total_vacc, color = "Baseline")) +
+      geom_line(data = country_cfact,
+                aes(x = as.Date(date), y = total_vacc, color = "Counterfactual")) +
+      xlab("Date") +
+      ylab("Total doses administered") +
+      scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6)) +
+      labs(color=country_iso)
 
     plot_prod_vs_vacc = ggplot() +
       geom_line(data = country_prod,
@@ -109,9 +106,11 @@ plot_cfact = function(cfact, baseline, production) {
       scale_y_continuous(labels = unit_format(unit = "B", scale = 1e-9)) +
       labs(color=country_iso)
 
+
     combined_plot =
       ggarrange(plot_dose1,
                 plot_dose2,
+                plot_total_dose,
                 plot_prod_vs_vacc,
                 nrow = 2,
                 ncol = 2)
@@ -122,10 +121,49 @@ plot_cfact = function(cfact, baseline, production) {
       size = 16
     ))
 
-    ggsave(paste0("counterfactual_plots/", country_iso, ".pdf"),
-           plot = combined_plot,
-           device = "pdf")
+    ggsave(paste0("counterfactual_plots/", country_iso, ".png"),
+           plot = combined_plot)
   })
+}
+
+# Plot how production caps vaccination for different shifts
+plot_together = function(baseline, production) {
+  countries_of_interest = c("USA", "GBR")
+  shifts = c(0,30,60,90)
+  walk(countries_of_interest, function(country_iso) {
+    plots = map(shifts, function(shift_by) {
+      country_vacc = baseline %>% filter(iso3c == country_iso)
+      country_prod = production %>% filter(iso3c == country_iso)
+      country_cfact = gen_cfact_with_prod(country_vacc, country_prod, shift_by) %>%
+        mutate(total_vacc = cumsum(first_doses + second_doses + third_doses))
+
+      plot_prod_vs_vacc = ggplot() +
+        geom_line(data = country_prod,
+                  aes(x = as.Date(date),
+                      y = cumulative_available_vaccines,
+                      color = "Production")) +
+        geom_line(data = country_cfact,
+                  aes(x = as.Date(date), y = total_vacc, color = "Vaccination")) +
+        xlab("Date") +
+        ylab("Cumulative total vaccines") +
+        scale_y_continuous(labels = unit_format(unit = "B", scale = 1e-9)) +
+        labs(color=paste0("Shift by ", shift_by, " days"))
+
+      return(plot_prod_vs_vacc)
+    })
+    combined_plot =
+      ggarrange(plotlist = plots,
+                nrow = 2,
+                ncol = 2)
+    combined_plot = annotate_figure(combined_plot, top = text_grob(
+      paste0("Binding of production constraint for different shifts for ", country_iso),
+      color = "black",
+      size = 16
+    ))
+    ggsave(paste0("shifts_plots/", country_iso, ".png"),
+           plot = combined_plot)
+  })
+
 }
 
 # read in real world vaccination series
@@ -150,6 +188,8 @@ cfact_with_prod = cfact_with_prod %>% mutate(shifted_by = shift_by)
 saveRDS(cfact_with_prod, "counterfactual_timelines/counterfactual_vaccination.Rds")
 plot_cfact(cfact_with_prod, base_vaccination, counterfactual_production)
 
+plot_together(base_vaccination, counterfactual_production)
+
 # create a counterfactual with no vaccines
 cfact_no_vaccines = base_vaccination %>%
   mutate(first_doses = 0, second_doses = 0, third_doses = 0)
@@ -157,7 +197,6 @@ saveRDS(cfact_no_vaccines, "counterfactual_timelines/no_vaccines.Rds")
 
 # save the base real scenario from OWID
 saveRDS(base_vaccination, "counterfactual_timelines/owid_raw.Rds")
-
 
 
 ### Check whether with unlimited production and no shift the counterfactual
